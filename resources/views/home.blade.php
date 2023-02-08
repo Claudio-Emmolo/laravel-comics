d64a1c86aac5f2a246db852be90969da20b56a81
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-02-08 15:41:12 +0100

    Create comics cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,19 @@
 
 @section('main-app')
 <main>
-    <h1>Ciao</h1>
+    <div class="container text-light">
+        <div class="row row-cols-6">
+            @foreach ($comicList as $comic)
+            <div class="col">
+                <div class="img-container w-100 h-50">
+                    <img src="{{$comic['thumb']}}" alt="" class="h-100 w-100">
+                </div>
+                <h3 class="fs-6 fw-bold text-uppercase">
+                    {{$comic['series']}}
+                </h3>
+            </div>
+            @endforeach
+        </div>
+    </div>
 </main>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $comicList = config('comics');
+    //dd($comicList);
+    return view('home', ['comicList' => $comicList]);
 })->name('homepage');
